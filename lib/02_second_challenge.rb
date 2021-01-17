6bf6d1a4b83a7bd5c4c4e977bf444049c02e3920
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-17 23:15:17 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
-      :matrirch => {},
+      :matriarch => {},
       :heroine => {},
-      :heroine_friends => [] 
-      }
-  }
+      :heroine_friends => []
+   }
+}
 
   
 end
